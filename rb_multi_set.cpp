f0a3ad1e8eabf310e82fe7c3110631a3dc5b23d0
Evgeny <je.mouth@gmail.com>
2017-04-17 00:20:06 +0300

    time test for set, no  output

--- rb_multi_set.cpp
+++ rb_multi_set.cpp
@@ -1,12 +1,19 @@
 #include "rb_multi_set.h"
-#include "time.h"
-#include "
+#include "ctime"
+#include "set"
 void time_test(){
-   time_t time;
+   time_t start;
    time_t time_passed;
+   double res;
+   std::set<int> st;
+   multiset<int> mst;
 
-
-
+   start = clock();
+   for(int i = 0; i< 10000;++i){
+           st.insert(rand());
+       }
+   time_passed = clock();
+   res = double(time_passed-start)/CLOCKS_PER_SEC;
 
 
 
